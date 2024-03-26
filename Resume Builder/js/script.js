91922850a9a2e1d527d6151b755171da74d08fc0
KetulPatel234 <160125439+KetulPatel234@users.noreply.github.com>
2024-03-26 20:00:47 +0530

    Updated

---
+++ Resume Builder/js/script.js
@@ -0,0 +1,18 @@
+function openFile(file_name) {
+    window.open(file_name)
+}
+
+
+function darkMode(dark_link, event) {
+    event.preventDefault();
+    var body = document.body;
+
+    if (body.classList.contains('dark-mode')) {
+        dark_link.innerHTML = 'Light Mode'
+        body.classList.remove("dark-mode")
+    }
+    else {
+        dark_link.innerHTML = 'Dark Mode'
+        body.classList.add("dark-mode")
+    }
+}
