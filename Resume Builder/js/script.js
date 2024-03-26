76ef59a80e49b3990de57a582741686e34c12a16
KetulPatel234 <160125439+KetulPatel234@users.noreply.github.com>
2024-03-26 20:18:48 +0530

    Updates

--- Resume Builder/js/script.js
+++ Resume Builder/js/script.js
@@ -8,11 +8,12 @@ function darkMode(dark_link, event) {
     var body = document.body;
 
     if (body.classList.contains('dark-mode')) {
-        dark_link.innerHTML = 'Light Mode'
+        dark_link.innerHTML = 'Dark Mode'
         body.classList.remove("dark-mode")
     }
+    
     else {
-        dark_link.innerHTML = 'Dark Mode'
+        dark_link.innerHTML = 'Light Mode'
         body.classList.add("dark-mode")
     }
 }
