e75d1aed817bb0ca81adabd38deec95422484a59
KetulPatel234 <160125439+KetulPatel234@users.noreply.github.com>
2024-03-27 19:39:38 +0530

    js_updated

--- Resume Builder/js/script.js
+++ Resume Builder/js/script.js
@@ -1,14 +1,17 @@
-function darkMode(dark_link, event) {
+function darkMode(event) {
     event.preventDefault();
     var body = document.body;
-
+    var dark1 = document.getElementById("dark_link_1");
+    var dark2 = document.getElementById("dark_link_2");
     if (body.classList.contains('dark-mode')) {
-        dark_link.innerHTML = 'Dark Mode'
+        dark1.innerHTML = 'Dark Mode'
+        dark2.innerHTML = 'Dark Mode'
         body.classList.remove("dark-mode")
     }
     
     else {
-        dark_link.innerHTML = 'Light Mode'
+        dark1.innerHTML = 'Light Mode'
+        dark2.innerHTML = 'Light Mode'
         body.classList.add("dark-mode")
     }
 }
